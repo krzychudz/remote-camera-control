3eefeb0a627f533b6051f84f0df9fb89d3cd4a6e
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2021-11-13 14:53:49 +0100

    Move widgets to separate build methods

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -15,73 +15,100 @@ class LoginScreen extends StatelessWidget {
               color: Colors.red,
             ),
           ),
-          Flexible(
+          const Flexible(
             flex: 2,
             child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Login',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: ElevatedButton(
-                      child: const Text(
-                        "Zaloguj",
-                      ),
-                      onPressed: () => {},
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: const TextSpan(
-                          text: "Nie masz konta?\n",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Zarejestruj się',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              padding: EdgeInsets.all(32.0),
+              child: LoginForm(),
             ),
           ),
         ],
       )),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  Widget _buildLoginFormField() {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Login',
+      ),
+    );
+  }
+
+  Widget _buildPasswordFormField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return SizedBox(
+      width: 200,
+      height: 50,
+      child: ElevatedButton(
+        child: const Text(
+          "Zaloguj",
+        ),
+        onPressed: () => {},
+      ),
+    );
+  }
+
+  RichText _buildRegisterSection() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: const TextSpan(
+        text: "Nie masz konta?\n",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+        ),
+        children: [
+          TextSpan(
+            text: 'Zarejestruj się',
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildLoginFormField(),
+        const SizedBox(
+          height: 16,
+        ),
+        _buildPasswordFormField(),
+        const SizedBox(
+          height: 32,
+        ),
+        _buildSubmitButton(),
+        const SizedBox(
+          height: 8,
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            child: _buildRegisterSection(),
+          ),
+        ),
+      ],
     );
   }
 }
