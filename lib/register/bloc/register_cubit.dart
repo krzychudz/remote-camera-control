b78f0a88409c412ce91031a719e7a7ee22e88d22
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2022-01-10 15:36:16 +0100

    Add email to registration form

--- lib/register/bloc/register_cubit.dart
+++ lib/register/bloc/register_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:app/common/model/email.dart';
 import 'package:app/common/model/password.dart';
 import 'package:app/common/model/username.dart';
 import 'package:app/register/bloc/register_state.dart';
@@ -18,7 +19,19 @@ class RegisterCubit extends Cubit<RegisterState> {
       state.copyWith(
         username: username,
         status: Formz.validate(
-          [state.password, username, state.rePassword],
+          [state.password, username, state.rePassword, state.email],
+        ),
+      ),
+    );
+  }
+
+  void onEmailChanged(String newEmailState) {
+    final email = Email.dirty(newEmailState);
+    emit(
+      state.copyWith(
+        email: email,
+        status: Formz.validate(
+          [state.password, email, state.rePassword, state.username],
         ),
       ),
     );
@@ -30,7 +43,7 @@ class RegisterCubit extends Cubit<RegisterState> {
       state.copyWith(
         password: password,
         status: Formz.validate(
-          [password, state.rePassword, state.username],
+          [password, state.rePassword, state.username, state.email],
         ),
       ),
     );
@@ -42,7 +55,7 @@ class RegisterCubit extends Cubit<RegisterState> {
       state.copyWith(
         rePassword: rePassword,
         status: Formz.validate(
-          [rePassword, state.password, state.username],
+          [rePassword, state.password, state.username, state.email],
         ),
       ),
     );
--- lib/register/bloc/register_state.dart
+++ lib/register/bloc/register_state.dart
@@ -1,3 +1,4 @@
+import 'package:app/common/model/email.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
 
@@ -8,27 +9,31 @@ class RegisterState extends Equatable {
   const RegisterState(
       {this.status = FormzStatus.pure,
       this.username = const Username.pure(),
+      this.email = const Email.pure(),
       this.password = const Password.pure(),
       this.rePassword = const Password.pure()});
 
   final FormzStatus status;
   final Username username;
+  final Email email;
   final Password password;
   final Password rePassword;
 
   RegisterState copyWith({
     FormzStatus? status,
     Username? username,
+    Email? email,
     Password? password,
     Password? rePassword,
   }) {
     return RegisterState(
         status: status ?? this.status,
         username: username ?? this.username,
+        email: email ?? this.email,
         password: password ?? this.password,
         rePassword: rePassword ?? this.rePassword);
   }
 
   @override
-  List<Object> get props => [status, username, password, rePassword];
+  List<Object> get props => [status, username, email, password, rePassword];
 }
